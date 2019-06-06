7c3b51191d680b879407c4524991e1ad27e94534
clovisd <clovisd@users.noreply.github.com>
2019-06-06 03:22:37 -0600

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -41,7 +41,7 @@ const siteConfig = {
     {doc: 'doc1', label: 'Docs'},
     {doc: 'doc4', label: 'Dev'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'News'},
+    {blog: false, label: 'News'},
   ],
 
   // If you have users set above, you add it here:
