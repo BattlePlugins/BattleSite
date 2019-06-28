0acd746cae3b8047304c0972f79d7ef17c8087d8
clovisd <clovisd@users.noreply.github.com>
2019-06-28 04:57:43 -0600

    Dynamic Avatar URLs

--- website/data/users.js
+++ website/data/users.js
@@ -1,28 +1,28 @@
 module.exports = [
   {
     caption: 'clovisd',
-    image: '/img/clovid.png',
+    image: 'https://avatars3.githubusercontent.com/u/1336589?s=96&v=4',
     infoLink: 'https://twitter.com/clovisdelmotte',
     pinned: true,
     bpActive: false,
   },
   {
     caption: 'Zach443',
-    image: '/img/users/zach443.png',
+    image: 'https://avatars1.githubusercontent.com/u/4268906?s=96&v=4',
     infoLink: 'https://twitter.com/zach443',
     pinned: false,
     bpActive: true,
   },
   {
     caption: 'lDucks',
-    image: '/img/users/ducks.png',
+    image: 'https://avatars2.githubusercontent.com/u/3073160?s=96&v=4',
     infoLink: 'https://twitter.com/lducks',
     pinned: true,
     bpActive: true,
   },
   {
     caption: 'Redned',
-    image: '/img/users/redned.png',
+    image: 'https://avatars2.githubusercontent.com/u/29153871?s=96&v=4',
     infoLink: 'https://twitter.com/redned',
     pinned: false,
     bpActive: true,
