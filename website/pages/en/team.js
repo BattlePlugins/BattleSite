7e89a01e753a31ff33e39fd7c6a566dc9d0495ce
clovisd <clovisd@users.noreply.github.com>
2019-06-28 04:34:06 -0600

    Update team.js

--- website/pages/en/team.js
+++ website/pages/en/team.js
@@ -16,9 +16,9 @@ class Users extends React.Component {
   render() {
     const {config: siteConfig} = this.props;
     const fbUsersToShowcase = siteConfig.users.filter(
-      user => user.fbOpenSource,
+      user => user.bpActive,
     );
-    const restToShowcase = siteConfig.users.filter(user => !user.fbOpenSource);
+    const restToShowcase = siteConfig.users.filter(user => !user.bpActive);
 
     return (
       <div className="mainContainer">
