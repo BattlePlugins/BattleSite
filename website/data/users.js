ce458adfc89e9dedcb6cc56db842386aa1b78b37
clovisd <clovisd@users.noreply.github.com>
2019-06-28 05:01:16 -0600

    Scaling Avatars

--- website/data/users.js
+++ website/data/users.js
@@ -1,28 +1,28 @@
 module.exports = [
   {
     caption: 'clovisd',
-    image: 'https://avatars3.githubusercontent.com/u/1336589?s=96&v=4',
+    image: 'https://avatars3.githubusercontent.com/u/1336589?s=250&v=4',
     infoLink: 'https://twitter.com/clovisdelmotte',
     pinned: true,
     bpActive: false,
   },
   {
     caption: 'Zach443',
-    image: 'https://avatars1.githubusercontent.com/u/4268906?s=96&v=4',
+    image: 'https://avatars1.githubusercontent.com/u/4268906?s=250&v=4',
     infoLink: 'https://twitter.com/zach443',
     pinned: false,
     bpActive: true,
   },
   {
     caption: 'lDucks',
-    image: 'https://avatars2.githubusercontent.com/u/3073160?s=96&v=4',
+    image: 'https://avatars2.githubusercontent.com/u/3073160?s=250&v=4',
     infoLink: 'https://twitter.com/lducks',
     pinned: true,
     bpActive: true,
   },
   {
     caption: 'Redned',
-    image: 'https://avatars2.githubusercontent.com/u/29153871?s=96&v=4',
+    image: 'https://avatars2.githubusercontent.com/u/29153871?s=250&v=4',
     infoLink: 'https://twitter.com/redned',
     pinned: false,
     bpActive: true,
