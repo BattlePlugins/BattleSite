6673e4955c4bdaf4c88f4159a422917a87fabc1c
clovisd <clovisd@users.noreply.github.com>
2019-06-29 03:39:19 -0600

    Updates and Adding Users

--- website/pages/en/team.js
+++ website/pages/en/team.js
@@ -20,23 +20,19 @@ class Users extends React.Component {
           <div className="showcaseSection">
             <h1>BattlePlugins Staff and Team Members</h1>
             <p>These are all the individuals who have made BattlePlugins what it is today. Without the help of those listed on this page and those to come, this community wouldn't be what it is today!</p>
-            <a href="https://github.com/BattlePlugins/BattleSite/blob/master/website/data/users.js" className="button">Update Page</a>
             <div className="prose">
               <h2>Active BattlePlugins Staff</h2>
-              <p>Current active members of the BattlePlugins team.</p>
             </div>
             <Showcase users={bpActiveStaff} />
               <div className="prose">
                 <h2>Retired BattlePlugins Staff</h2>
-                <p>Past and Vetern members of the BattlePlugins team who are no longer active.</p>
               </div>
             <Showcase users={restToShowcase} />
             <br></br>
             <div className="prose">
               <h4>Interested in Contributing?</h4>
-              <a href="https://docs.battleplugins.org/docs/guidelines" className="button">
-              Get Started Today
-              </a>
+              <a href="https://docs.battleplugins.org/docs/guidelines" className="button">Get Started Today</a>
+              <a href="https://github.com/BattlePlugins/BattleSite/blob/master/website/data/users.js" className="button">Update Users</a>
             </div>
           </div>
         </Container>
