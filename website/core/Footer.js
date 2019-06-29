af7180b590c606873853d9c25bacb0c64b085d8b
clovisd <clovisd@users.noreply.github.com>
2019-06-29 04:51:57 -0600

    footer tests

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -36,40 +36,35 @@ class Footer extends React.Component {
             )}
           </a>
           <div>
-            <h5>Docs Quick Start:</h5>
-            <a href={this.docUrl('ba.html', this.props.language)}>
-              BattleArena Docs
-            </a>
-            <a href={this.docUrl('bt.html', this.props.language)}>
-              BattleTracker Docs
-            </a>
-            <a href={this.docUrl('dev.html', this.props.language)}>
-              API and Development Docs
-            </a>
-            <a href={this.docUrl('other.html', this.props.language)}>
-              Other Plugins & Docs
-            </a>
-          </div>
-          <div>
             <h5>Organization Info:</h5>
-            <a href="https://discord.gg/tMVPVJf"><i class="fab icon-white fa-discord"/> Discord Support</a>
+            <a href="https://battleplugins.org/"><img src="https://battleplugins.org/favicon.ico" alt="Icon" height="20"/> Website</a>
+            <a href="https://discord.gg/tMVPVJf"><i class="fab icon-white fa-discord"/> Discord Server</a>
             <a href="https://github.org/BattlePlugins"><i class="fab icon-white fa-github-square"/> Github Org</a>
-            <a href="https://battleplugins.org/"><i class="fas icon-white fa-desktop"/> Website</a>
-            <a href="https://wiki.battleplugins.org/"><i class="fas icon-white fa-scroll"/> Legacy Wiki</a>
+            <a href="https://ci.battleplugins.org/"><img src="https://ci.battleplugins.org/favicon.ico" alt="Icon" height="20"/> Jenkins Server</a>
+            <a href="https://repo.battleplugins.org/"><<img src="https://repo.battleplugins.org/favicon.ico" alt="Icon" height="20"/> Public Repo</a>
           </div>
           <div>
-            <h5>About the Docs:</h5>
-            <a href="https://github.com/BattlePlugins/BattleDocs">Contribute to the Docs Today</a>
+            <h5>Docs Quick Start:</h5>
+            <a href="https://docs.battleplugins.org/docs/"><i class="fab icon-white fa-desktop"/> Documentation Home</a>
+            <a href="https://docs.battleplugins.org/docs/ba"><i class="icon-white fab fa-swords"/> BattleArena Docs</a>
+            <a href="https://docs.battleplugins.org/docs/bt"><i class="icon-white fab fa-chart-area"/> BattleTracker Docs</a>
+            <a href="https://docs.battleplugins.org/docs/vp"><i class="icon-white fab fa-user-friends"/> VirtualPlayer Docs</a>
+            <a href="https://wiki.battleplugins.org/"><i class="fab icon-white fa-scroll"/> Legacy Wiki</a>
+          </div>
+          <div>
+            <h5>Website Info:</h5>
+            <a href="https://github.com/BattlePlugins/BattleDocs">Open-Source</a>
             <a href="http://ci.battleplugins.org/jobs/BattleDocs">Build Status: <img alt="Jenkins" src="https://img.shields.io/jenkins/build/http/s14.cl6.us:8080/job/BattleDocs.svg?style=flat-square"/></a>
             <a href="https://github.com/BattlePlugins/BattleDocs">Doc Size: <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/battleplugins/battledocs.svg?style=flat-square"/></a>
             <a href="https://github.com/BattlePlugins/BattleDocs/pulls">Pending Updates: <img alt="GitHub pull requests" src="https://img.shields.io/github/issues-pr/battleplugins/battledocs.svg?style=flat-square"/></a>
             <br></br>
-            <img alt="GitHub followers" src="https://img.shields.io/github/followers/battleplugins.svg?label=Follow&style=social"/> | <img alt="GitHub stars" src="https://img.shields.io/github/stars/battleplugins/battledocs.svg?style=social"/> | <img alt="GitHub watchers" src="https://img.shields.io/github/watchers/battleplugins/battledocs.svg?label=Watch&style=social"/>
+
           </div>
         </section>
 		<div>
 			<center>
 			<h5 className="footer-slogan">Hosted By:</h5>
+          <img alt="GitHub followers" src="https://img.shields.io/github/followers/battleplugins.svg?label=Follow&style=social"/> | <img alt="GitHub stars" src="https://img.shields.io/github/stars/battleplugins/battledocs.svg?style=social"/> | <img alt="GitHub watchers" src="https://img.shields.io/github/watchers/battleplugins/battledocs.svg?label=Watch&style=social"/>
         	<a href="https://cl6web.com" className="footer-slogan"><img src={`${this.props.config.baseUrl}img/cl6.png`} alt="CL6 Hosting" height="60"/></a>
         	</center>
         </div>
