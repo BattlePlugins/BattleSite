cc478723fee77c549cf4c8018537905492b9167d
clovisd <clovisd@users.noreply.github.com>
2019-06-29 05:18:31 -0600

    Update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,15 +37,15 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Organization Info:</h5>
-            <a href="https://battleplugins.org/"><img src="https://battleplugins.org/favicon.ico" alt="Icon" height="20"/> Website</a>
+            <a href="https://battleplugins.org/"><img src="https://battleplugins.org/img/favicon.ico" alt="Icon" height="15"/> Website</a>
             <a href="https://discord.gg/tMVPVJf"><i class="fab icon-white fa-discord"/> Discord Server</a>
             <a href="https://github.org/BattlePlugins"><i class="fab icon-white fa-github-square"/> Github Org</a>
-            <a href="https://ci.battleplugins.org/"><img src="https://ci.battleplugins.org/favicon.ico" alt="Icon" height="20"/> Jenkins Server</a>
-            <a href="https://repo.battleplugins.org/"><img src="https://repo.battleplugins.org/favicon.ico" alt="Icon" height="20"/> Public Repo</a>
+            <a href="https://ci.battleplugins.org/"><img src="https://ci.battleplugins.org/favicon.ico" alt="Icon" height="10"/> Jenkins Server</a>
+            <a href="https://repo.battleplugins.org/"><img src="https://repo.battleplugins.org/favicon.ico" alt="Icon" height="15"/> Public Repo</a>
           </div>
           <div>
             <h5>Docs Quick Start:</h5>
-            <a href="https://docs.battleplugins.org/docs/"><i class="fab icon-white fa-desktop"/> Documentation Home</a>
+            <a href="https://docs.battleplugins.org/docs/"><i class="fas icon-white fa-desktop"/> Documentation Home</a>
             <a href="https://docs.battleplugins.org/docs/ba"><i class="fas icon-white fa-swords"/> BattleArena Docs</a>
             <a href="https://docs.battleplugins.org/docs/bt"><i class="fas icon-white fa-chart-area"/> BattleTracker Docs</a>
             <a href="https://docs.battleplugins.org/docs/vp"><i class="fas icon-white fa-user-friends"/> VirtualPlayer Docs</a>
@@ -65,7 +65,8 @@ class Footer extends React.Component {
 			<center>
 			<h5 className="footer-slogan">Hosted By:</h5>
           <img alt="GitHub followers" src="https://img.shields.io/github/followers/battleplugins.svg?label=Follow&style=social"/> | <img alt="GitHub stars" src="https://img.shields.io/github/stars/battleplugins/battledocs.svg?style=social"/> | <img alt="GitHub watchers" src="https://img.shields.io/github/watchers/battleplugins/battledocs.svg?label=Watch&style=social"/>
-        	<a href="https://cl6web.com" className="footer-slogan"><img src={`${this.props.config.baseUrl}img/cl6.png`} alt="CL6 Hosting" height="60"/></a>
+          <br></br>
+          <a href="https://cl6web.com" className="footer-slogan"><img src={`${this.props.config.baseUrl}img/cl6.png`} alt="CL6 Hosting" height="60"/></a>
         	</center>
         </div>
         <section className="copyright">{this.props.config.copyright}</section>
